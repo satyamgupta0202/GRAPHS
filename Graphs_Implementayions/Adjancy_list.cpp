9b745553e8a84baa7dfcaec1fc4f0a9566a4edb9
satyam <2019335@iiitdmj.ac.in.com>
2021-03-07 18:29:37 +0530

    Adjancy_list

---
+++ Graphs_Implementayions/Adjancy_list.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class graph {
+
+    int v;
+    list<int>*l;
+
+    public:
+    graph(int v){
+        this->v=v;
+        l = new list<int>[v];
+    }
+    void addedge(int x,int y){
+        l[x].push_back(y);
+        l[y].push_back(x);
+    }
+    void print(){
+        for(int i=0;i<v;i++){
+            cout<<"node ->"<<i<<":";
+            for(auto child : l[i]){
+                cout<<child<<"-->>";
+            }
+            cout<<endl;
+        }
+    }
+};
+
+
+
+
+
+
+
+
+int main(){
+
+    graph g(4);
+    g.addedge(0,1);
+    g.addedge(0,2);
+    g.addedge(2,3);
+    g.addedge(1,3);
+    g.print();
+    return 0;
+}
